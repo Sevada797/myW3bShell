dbb32a5494aa72e1b0442aa3b440dd52ded8298e
Sevada <64130935+Sevada797@users.noreply.github.com>
2024-01-23 01:42:24 +0400

    Update ngrok.cmd

--- windows/ngrok.cmd
+++ windows/ngrok.cmd
@@ -2,5 +2,7 @@ REM  COMMENT SECTION
 REM  I got this free authtoken from this list here https://gist.github.com/aels/4b26aa8bd9db8108e171ffbe81cdf776
 REM  in case of ngrok failing try other tokens or register at ngrok.com and get free authtoken
 
-%cd%\ngrok\ngrok.exe config add-authtoken 26p3rvZocfT7VzraejQNoCjPNN6_6UqEGG1izGcbE8ewtBchT
-%cd%\ngrok\ngrok.exe http 7797
+echo %cd%\ngrok.exe config add-authtoken 26p3rvZocfT7VzraejQNoCjPNN6_6UqEGG1izGcbE8ewtBchT >actual.cmd
+echo %cd%\ngrok.exe http 7797 >>actual.cmd
+%cd%\actual.cmd
+
