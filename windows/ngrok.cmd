9968bf07d23c9be82727ed52e89393740cfcebfe
Sevada <64130935+Sevada797@users.noreply.github.com>
2024-01-23 01:43:30 +0400

    Delete windows/ngrok.cmd

--- windows/ngrok.cmd
+++
@@ -1,8 +0,0 @@
-REM  COMMENT SECTION
-REM  I got this free authtoken from this list here https://gist.github.com/aels/4b26aa8bd9db8108e171ffbe81cdf776
-REM  in case of ngrok failing try other tokens or register at ngrok.com and get free authtoken
-
-echo %cd%\ngrok.exe config add-authtoken 26p3rvZocfT7VzraejQNoCjPNN6_6UqEGG1izGcbE8ewtBchT >actual.cmd
-echo %cd%\ngrok.exe http 7797 >>actual.cmd
-%cd%\actual.cmd
-
