8c46243df836de14b6c05a7d757714a71068db8c
Sevada <64130935+Sevada797@users.noreply.github.com>
2024-01-24 07:32:58 +0400

    Update setup.cmd

--- windows/setup.cmd
+++ windows/setup.cmd
@@ -4,13 +4,13 @@ curl https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-windows-amd64.zip -o n
 tar -xf %cd%\ngrok.zip
 
 REM Download and install PHP
-curl https://windows.php.net/downloads/releases/php-devel-pack-8.3.2-Win32-vs16-x64.zip -o php.zip
+curl https://windows.php.net/downloads/releases/archives/php-7.2.1-Win32-VC15-x64.zip -o php.zip
 tar -xf %cd%\php.zip
 
 REM  COMMENT SECTION
 REM  I got this free authtoken from this list here https://gist.github.com/aels/4b26aa8bd9db8108e171ffbe81cdf776
 REM  in case of ngrok failing try other tokens or register at ngrok.com and get free authtoken
-REM  start hidden ngrok using /B option
+REM  start things hidden using /B option
 echo %cd%\ngrok.exe config add-authtoken 26p3rvZocfT7VzraejQNoCjPNN6_6UqEGG1izGcbE8ewtBchT >actual.cmd
 echo %cd%\ngrok.exe http 7797 >>actual.cmd
 start cmd /K %cd%\actual.cmd
